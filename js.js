8f7ca4e2f55d4f15ff28198dc8d820387f513530
jmenadesign <javascript@ZiMac4-2.local>
2019-01-12 14:39:49 +0100

    another test

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
+/*
 let key='e2caab4e544fbb5e';
-let Weather = "https://api.wunderground.com/api/" + key +"/forecast/geolookup/conditions/lang:SP/q/" + "40.7127837" + "," + "-74.00594130000002" + ".json";
-
-let myUrl = 'https://api.wunderground.com/api/e2caab4e544fbb5e/forecast/geolookup/conditions/lang:SP/q/40.7127837,-74.00594130000002.json'
-
+let Weather = "https://api.wunderground.com/api/" + key +"/forecast/geolookup/conditions/lang:SP/q/" + "40.7127837" + "," + "-74.00594130000002" + ".json"
 
 const apiUrl = Weather
 
 async function callApi(){
-    let temperatura = await fetch(myUrl)
+    let temperatura = await fetch(apiUrl)
     console.log(temperatura)
     temperatura = await temperatura.json()
     console.log(temperatura.city)
@@ -22,4 +20,33 @@ let dameTemperatura = document.getElementById('boton')
 
 dameTemperatura.addEventListener('click',function(){
     document.getElementById("mostrarInfo").innerHTML = "'${conditions.city}'";
+})
+*/
+
+//weather = 'https://api.wunderground.com/api/e2caab4e544fbb5e/forecast/geolookup/conditions/lang:SP/q/40.42273,-3.7130593000000003.json'
+
+let key='e2caab4e544fbb5e';
+let weather = "https://api.wunderground.com/api/" + key +"/forecast/geolookup/conditions/lang:SP/q/" + "40.7127837" + "," + "-74.00594130000002" + ".json"
+
+
+
+
+async function llamadaApi(){
+
+let characters = await fetch(weather)
+
+characters = await characters.json()
+
+console.log(characters) // 0 a 9 name y population
+
+//representar(characters)
+
+}
+
+llamadaApi()
+
+let dameTemperatura = document.getElementById('boton')
+
+dameTemperatura.addEventListener('click',function(){
+    document.getElementById("mostrarInfo").innerHTML = `${conditions.city}`
 })
