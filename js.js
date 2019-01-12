bc7d29592e2c1b4a24d69fb649424a9b09b6e1e4
jmenadesign <javascript@ZiMac4-2.local>
2019-01-12 13:03:40 +0100

    cors :(

--- js.js
+++ js.js
@@ -1,3 +1,21 @@
-const apiUrl = 'https://www.metaweather.com/api/location'
+const apiUrl = 'https://www.metaweather.com/api/location/search/?query='
 
 
+async function callApi(){
+    let temperatura = await fetch(apiUrl)
+    temperatura = await temperatura.json()
+    console.log(temperatura.location_type)
+    representar(temperatura.location_type)
+}
+
+callApi()
+
+
+
+let dameTemperatura = document.getElementById('boton')
+
+
+
+dameTemperatura.addEventListener('click',function(){
+    
+})
