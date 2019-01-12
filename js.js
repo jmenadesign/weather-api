afa2ffb08496ace1007ccf459b520cb5ce9b750c
jmenadesign <javascript@ZiMac4-2.local>
2019-01-12 14:14:38 +0100

    trying to avoid cors

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
-const apiUrl = 'https://www.metaweather.com/api/location/search/?query='
+let key='e2caab4e544fbb5e';
+let Weather = "https://api.wunderground.com/api/" + key +"/forecast/geolookup/conditions/lang:SP/q/" + "40.7127837" + "," + "-74.00594130000002" + ".json";
 
+let myUrl = 'https://api.wunderground.com/api/e2caab4e544fbb5e/forecast/geolookup/conditions/lang:SP/q/40.7127837,-74.00594130000002.json'
+
+
+const apiUrl = Weather
 
 async function callApi(){
-    let temperatura = await fetch(apiUrl)
+    let temperatura = await fetch(myUrl)
+    console.log(temperatura)
     temperatura = await temperatura.json()
-    console.log(temperatura.location_type)
-    representar(temperatura.location_type)
+    console.log(temperatura.city)
+    //representar(temperatura.city)
 }
 
 callApi()
@@ -14,8 +20,6 @@ callApi()
 
 let dameTemperatura = document.getElementById('boton')
 
-
-
 dameTemperatura.addEventListener('click',function(){
-    
+    document.getElementById("mostrarInfo").innerHTML = "'${conditions.city}'";
 })
